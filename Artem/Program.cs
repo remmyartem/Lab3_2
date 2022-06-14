da199bfcd3525ac4ddd277935194a34b6d9ba530
remmyartem <trosteniuk.artem1121@vu.cdu.edu.ua>
2022-06-14 13:28:44 +0300

    First commit

---
+++ Artem/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    partial class Program
+    {
+        static Student[] ReadData()
+        {
+            int count = System.IO.File.ReadAllLines("input.txt").Length;
+            Student[] studs = new Student[count];
+            StreamReader read = new StreamReader("input.txt");
+            for (int i = 0; i < count; i++)
+            {
+                studs[i] = new Student(read.ReadLine());
+            }
+            return studs;
+        }
+
+        static void runMenu(Student[] studs)
+        {
+            double sum = 0;
+            double average = 0;
+            for (int i = 0; i < studs.Length; i++)
+            {
+                sum = 0;
+                char[] arr ={studs[i].mathematicsMark,studs[i].physicsMark,studs[i].informaticsMark};
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == '-')
+                    {
+                        arr[j] = '2';
+                    }
+                    sum += (int)Char.GetNumericValue(arr[j]);
+                }
+                average = sum / arr.Length;
+                if (average>4.5)
+                {
+                    Console.WriteLine($"{studs[i].surName} {studs[i].firstName} {studs[i].patronymic} {average}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Результат виконання");
+            runMenu(ReadData());
+        }
+    }
+}
---
+++ Artem/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string withAllData)
+        {
+            string[] array = withAllData.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            surName = array[0];
+            firstName = array[1];
+            patronymic = array[2];
+            sex = char.Parse(array[3]);
+            dateOfBirth = array[4];
+            mathematicsMark = char.Parse(array[5]);
+            physicsMark = char.Parse(array[6]);
+            informaticsMark = char.Parse(array[7]);
+            scholarship = int.Parse(array[8]);
+        }
+    }
+}
